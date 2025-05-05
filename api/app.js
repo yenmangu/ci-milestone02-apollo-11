a41f730c4a2e8dc0b5504d61f7ac76da7256ef09
yenmangu <robert.shelford@googlemail.com>
2025-05-05 11:11:34 +0100

    Starting to deploy API to lightsail

--- api/app.js
+++ api/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 import { apiRouter } from './routes/route.js';
 const app = express();
 
+app.use(cors());
 app.use('/api', apiRouter);
 
 export default app;
--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ import app from './app.js';
 import 'dotenv/config';
 
 const PORT = process.env.PORT || 3000;
+// console.log(`PORT: ${process.env.PORT}`);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);

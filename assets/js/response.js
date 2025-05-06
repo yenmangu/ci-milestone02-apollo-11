dfbd5e26976f42e2b12fb4d6fe33df2995bd1635
yenmangu <robert.shelford@googlemail.com>
2025-05-06 16:32:54 +0100

    Move inline script to external response.js. Implement try-catch for better error handling.

--- assets/js/response.js
+++ assets/js/response.js
@@ -1,1 +1,19 @@
+const apiUrl = 'https://www.api.yenmangu.me/apollo/';
+const audioPath = 'audio';
 
+// async needed here to ensure try/catch and await works correctly
+document.addEventListener('DOMContentLoaded', async () => {
+	const responseDiv = document.getElementById('response');
+	const audioApi = apiUrl + audioPath;
+
+	try {
+		console.log('audioApi: ', audioApi);
+
+		const response = await fetch(audioApi);
+		const data = await response.json();
+		responseDiv ? (responseDiv.innerText = JSON.stringify(data)) : '';
+	} catch (error) {
+		console.error('Error fetching API data: ', error);
+		responseDiv ? (responseDiv.innerText = error) : '';
+	}
+});

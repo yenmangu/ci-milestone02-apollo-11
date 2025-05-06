c8e407a7b149129be1c058df085f2ac69145ea6e
yenmangu <robert.shelford@googlemail.com>
2025-05-06 02:55:01 +0100

    Show response data from apollo api on index.html

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { apiRouter } from './routes/route.js';
 const app = express();
 
+console.log('apiRouter: ', apiRouter);
 app.use(cors());
 app.use('/apollo', apiRouter);
 

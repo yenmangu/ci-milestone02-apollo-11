4c5a589def844e480c09370d79e06ab17c0fd6da
yenmangu <robert.shelford@googlemail.com>
2025-05-07 02:02:30 +0100

    Start TDD for CORS origin checking. Add env.js to encapsulate environment variables

---
+++ api/utils/env.js
@@ -0,0 +1,4 @@
+export const GITHUB_PAGES = process.env.GITHUB_PAGES;
+export const YENMANGU_ME = process.env.YENMANGU_ME;
+export const LOCAL = process.env.LOCAL;
+export const LOCALHOST = process.env.LOCALHOST;
---
+++ api/utils/tests/origin.test.js
@@ -0,0 +1,3 @@
+import { isTrustedOrigin } from '../origin.js';
+
+beforeEach(() => {});

cb7fcbaffd7736acf772b5d5db9f02aca84193d9
yenmangu <robert.shelford@googlemail.com>
2025-05-11 16:11:25 +0100

    Add 7 segment display font

--- assets/js/renderUI.js
+++ assets/js/renderUI.js
@@ -14,14 +14,13 @@ export function renderResponseData(responseData) {
 }
 
 export function testDskyPushButtons() {
-	const buttons = document.getElementsByClassName('push-button');
-	Array.from(buttons).forEach(button => {
+	/** @type {NodeListOf<HTMLElement>} */
+	const buttons = document.querySelectorAll('button.push-button');
+	buttons.forEach(button => {
 		button.addEventListener('click', e => {
 			e.preventDefault();
-			if (button instanceof HTMLElement) {
-				const dskyData = button.dataset.dsky;
-				console.log(`Button pressed: ${dskyData}`);
-			}
+			const dskyData = button.dataset.dsky;
+			console.log(`Button pressed: ${dskyData}`);
 		});
 	});
 }

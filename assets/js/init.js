c809d8f70e597472c443ee1eef3c41e8204188ce
yenmangu <robert.shelford@googlemail.com>
2025-05-12 18:24:17 +0100

    Add init.js to encapsulate initialisation

---
+++ assets/js/init.js
@@ -0,0 +1,17 @@
+import createKeypadStateManager from './keypad/keypadStateManager.js';
+import { initSevenSegmentDisplay } from './seven-segment/initSevenSegment.js';
+import { SevenSegmentDisplay } from './seven-segment/sevenSegment.js';
+import { initKeypadUI } from './userInput.js';
+/**
+ * Initialises the seven digit display and the keypad state manager factory
+ */
+export function initProgram() {
+	try {
+		const dskyDisplay = new SevenSegmentDisplay(initSevenSegmentDisplay());
+		const keypadStateManager = createKeypadStateManager(dskyDisplay);
+		initKeypadUI(keypadStateManager);
+		return keypadStateManager;
+	} catch (error) {
+		throw error;
+	}
+}

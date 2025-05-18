43604b4b3bec885e9ad437e665b3593c0b10e754
yenmangu <robert.shelford@googlemail.com>
2025-05-18 21:46:24 +0100

    Update JSDoc to enforce type safety

--- src/state/fsm.js
+++ src/state/fsm.js
@@ -1,6 +1,7 @@
 import {} from '../types/dskyTypes.js';
 import { GameController } from 'src/controller/gameController.js';
 import { MissionState } from 'src/mission/missionState.js';
+import { States } from './states.js';
 
 export class FSM {
 	/**
@@ -10,18 +11,28 @@ export class FSM {
 	constructor(gameController) {
 		this.game = gameController;
 		this.states = new Map();
-		/**
-		 * @type {}
-		 */
+		// /**
+		//  * @type {}
+		//  */
 		this.currentState = undefined;
 	}
 	/**
-	 * Adds a new mission state to the finite state machine
+	 * Adds a new mission state to the finite state machine.
 	 *
-	 * @param {string} name - The unique identifier for the state.
+	 * @param {keyof import('./states.js').StatesEnum} name - The key used to identify this state (e.g, "DESCENT").
+	 * Must be key from States enum.
 	 * @param {import('src/types/missionTypes.js').MissionStateContructor} stateClass - A class constructor for the mission state.
+	 * @example
+	 * fsm.addState("DESCENT", PoweredDescentState); // Now accessible via transitionTo("DESCENT")
 	 */
 	addState(name, stateClass) {
+		// Enforce valid state keys
+		if (!(name in States)) {
+			throw new TypeError(
+				`"${name}" is not a valid state key. Use ${Object.keys(States).join(', ')}`
+			);
+		}
+		// Runtime validation for class
 		if (!(stateClass.prototype instanceof MissionState)) {
 			throw new TypeError(`${stateClass.name} must inherit from MissionState`);
 		}

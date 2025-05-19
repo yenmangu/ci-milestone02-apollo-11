e25dacb25a373e99d27d905e2deae8864d934539
yenmangu <robert.shelford@googlemail.com>
2025-05-19 15:58:56 +0100

    Update index.html to display api response

--- src/env.js
+++ src/env.js
@@ -5,6 +5,7 @@ export const LOCALHOST = 'http://localhost';
 export const LOCAL_IP = 'http://127.0.0.1';
 
 export const SIMULATOR_PATH = '/simulator.html';
+export const HOME_PATH = '/index.html';
 
 export const AUDIO_PATH = 'audio';
 export const IMAGE_PATH = 'image/';
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import { getTestResponse } from './api.js';
 import { initProgram } from './init.js';
 import path from './domain.js';
-import { SIMULATOR_PATH } from './env.js';
+import { HOME_PATH, SIMULATOR_PATH } from './env.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
-	const testResponseData = await getTestResponse();
 	// Only load initProgram on simulator.html
 	console.log('pathname: ', path);
 	console.log('Simulator Path: ', SIMULATOR_PATH);
+
+	if (path === HOME_PATH) {
+		const testResponseData = await getTestResponse();
+		document.getElementById('home-response').innerText = testResponseData;
+	}
 
 	if (path === SIMULATOR_PATH) {
 		console.log('Awaiting initProgram');

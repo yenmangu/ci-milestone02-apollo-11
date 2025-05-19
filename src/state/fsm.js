02d4ef32048d8b4a1b658a08951eda5285bf386c
yenmangu <robert.shelford@googlemail.com>
2025-05-19 11:25:00 +0100

    Update typedef and corresponding implementations to use key

--- src/state/fsm.js
+++ src/state/fsm.js
@@ -2,6 +2,10 @@ import {} from '../types/dskyTypes.js';
 import { MissionState } from 'src/mission/missionState.js';
 import { AppStates } from 'src/types/missionTypes.js';
 
+/**
+ * @typedef {import('src/types/missionTypes.js').AppStatesKeys} AppStatesKey
+ */
+
 export class FSM {
 	/**
 	 *
@@ -13,12 +17,12 @@ export class FSM {
 		// /**
 		//  * @type {}
 		//  */
-		this.currentState = undefined;
+		this.currentState = null;
 	}
 	/**
 	 * Adds a new mission state to the finite state machine.
 	 *
-	 * @param {import('src/types/missionTypes.js').AppStatesKeys} name
+	 * @param {AppStatesKey} key
 	 * 	- The key used to identify this state (e.g, "DESCENT").
 	 * Must be key from States enum.
 	 * @param {import('src/types/missionTypes.js').MissionStateContructor} stateClass
@@ -27,11 +31,11 @@ export class FSM {
 	 * fsm.addState("DESCENT", PoweredDescentState);
 	 * 	// Now accessible via transitionTo("DESCENT")
 	 */
-	addState(name, stateClass) {
+	addState(key, stateClass) {
 		// Enforce valid state keys
-		if (!(name in AppStates)) {
+		if (!(key in AppStates)) {
 			throw new TypeError(
-				`"${name}" is not a valid state key. Use ${Object.keys(AppStates).join(
+				`"${key}" is not a valid state key. Use ${Object.keys(AppStates).join(
 					', '
 				)}`
 			);
@@ -40,17 +44,17 @@ export class FSM {
 		if (!(stateClass.prototype instanceof MissionState)) {
 			throw new TypeError(`${stateClass.name} must inherit from MissionState`);
 		}
-		const stateValue = AppStates[name];
+		const stateValue = AppStates[key];
 		this.states.set(stateValue, new stateClass(this.game));
 	}
 	/**
 	 *
-	 * @param {string} stateName
+	 * @param {AppStatesKey} key
 	 */
-	transitionTo(stateName) {
-		const state = this.states.get(AppStates[stateName]);
+	transitionTo(key) {
+		const state = this.states.get(AppStates[key]);
 		if (!state) {
-			throw new Error(`State "${stateName}" not found`);
+			throw new Error(`State "${key}" not found`);
 		}
 		if (this.currentState) this.currentState.exit();
 		this.currentState = state;

a883fe48c2e037926c8fbeebae37a5c1f21a2906
yenmangu <robert.shelford@googlemail.com>
2025-05-29 11:46:19 +0100

    Add utility helper for action watching

---
+++ src/util/watchUntilComplete.js
@@ -0,0 +1,24 @@
+import { actionEmitter } from '../event/eventBus.js';
+
+/**
+ * Subscribes to all 'action' events untill 'actionsComplete' is received.
+ * Automatically unsubscribes both listeners upon completion,
+ *
+ * @param {(event: {type: string, [key: string]: any}) => void} onAction
+ * @param {(event: {type: string, [key: string]: any}) => void} onComplete
+ * @returns {{unsubscribe: () => void}}
+ */
+export default function watchUntilComplete(onAction, onComplete) {
+	const actionSub = actionEmitter.on('action', onAction);
+	const completeSub = actionEmitter.on('actionsComplete', event => {
+		completeSub.unsubscribe();
+		actionSub.unsubscribe();
+		onComplete(event);
+	});
+	return {
+		unsubscribe: () => {
+			actionSub.unsubscribe();
+			completeSub.unsubscribe();
+		}
+	};
+}

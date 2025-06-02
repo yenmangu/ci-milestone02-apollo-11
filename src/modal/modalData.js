6141031a4f193e21a6c92c02a6b1b86e12a950cb
yenmangu <robert.shelford@googlemail.com>
2025-06-02 11:42:07 +0100

    Add modal data

---
+++ src/modal/modalData.js
@@ -0,0 +1,11 @@
+const instructionData = {
+	doi: {
+		line_1: `The crew are now awaiting the Apollo Guidance Computer (AGC)
+    to initiate the Descent Orbit Insertion burn.`,
+		line_2: `Please verify the burn below, which will skip forward
+    to a few seconds before the actual Burn Ignition Ground Elapsed Time (GET)
+    of "101:36:14", and watch the DSKY initiate Program 63 (P63).`
+	}
+};
+
+export { instructionData };

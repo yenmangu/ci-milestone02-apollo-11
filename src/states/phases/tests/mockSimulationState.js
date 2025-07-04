4ad7dec8e01812d7e05745ab07e2142b3b1355cd
yenmangu <robert.shelford@googlemail.com>
2025-07-04 21:08:30 +0100

    Update mockSimulationState factory to work as a resuable mocking factory

--- src/states/phases/tests/mockSimulationState.js
+++ src/states/phases/tests/mockSimulationState.js
@@ -1,14 +1,22 @@
 import { jest } from '@jest/globals';
 
 /**
+ * @typedef {import('../../simulationState.js').SimulationState} SimulationState
+ * @typedef {import('../../../types/runtimeTypes.js').RuntimePhase} RuntimePhase
+ * @typedef {import('../../../types/runtimeTypes.js').RuntimeCue} RuntimeCue
+ */
+
+const DEFAULT_MOCK_PHASE_ID = 'test_phase';
+
+/**
  * @param {Object} [overrides]
- * @returns {import('../../simulationState.js').SimulationState}
+ * @returns {SimulationState}
  */
 export function createMockSimulationState(overrides = {}) {
 	const playedCues = new Set();
 
 	/** @type {import('../../simulationState.js').SimulationState} */ const state = {
-		currentPhaseId: 'test_phase',
+		currentPhaseId: DEFAULT_MOCK_PHASE_ID,
 		currentGet: '000:00:00',
 		currentPhase: null,
 		playedCues,
@@ -30,4 +38,60 @@ export function createMockSimulationState(overrides = {}) {
 		...overrides
 	};
 	return state;
+}
+
+/**
+ *
+ * @param {Object} cueOverrides
+ * @returns {RuntimeCue}
+ */
+export function createMockCue(cueOverrides = {}) {
+	/** @type {RuntimeCue} */ const mockCue = {
+		get: '000:00:00',
+		getSeconds: 0,
+		key: 'test_cue_01',
+		data: {},
+		...cueOverrides
+	};
+	return mockCue;
+}
+
+/**
+ * @typedef {Object} Overrides
+ * @property {Object} runtimeOverrides
+ * @property {Object} cueOverrides
+ */
+/**
+ *
+ * @param {Overrides} [overrides]
+ * @returns {RuntimePhase}
+ */
+
+export function createMockMetadata(overrides) {
+	const { runtimeOverrides, cueOverrides } = overrides;
+	const mockCue = createMockCue(cueOverrides);
+
+	/** @type {RuntimePhase} */ const metaData = {
+		phaseId: DEFAULT_MOCK_PHASE_ID,
+		phaseName: 'Test Phase',
+		description: 'Test phase used for jest testing',
+		startGET: '000:00:00',
+		initialState: {
+			velocity: 0,
+			v_units: '',
+			altitude: { miles: 0, feet: 0 },
+			fuel: 100
+		},
+		endState: {
+			velocity: 0,
+			v_units: '',
+			altitude: { miles: 0, feet: 0 },
+			fuel: 100
+		},
+		cuesByKey: {
+			[mockCue.key]: mockCue
+		},
+		...runtimeOverrides
+	};
+	return metaData;
 }

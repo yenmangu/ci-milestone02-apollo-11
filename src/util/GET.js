ec424efe30d173ee91f5e1c40256cf20edbef18d
yenmangu <robert.shelford@googlemail.com>
2025-07-04 19:38:47 +0100

    update JSDoc for GET util

--- src/util/GET.js
+++ src/util/GET.js
@@ -1,8 +1,20 @@
+/**
+ * Convert a GET string e.g. ("102:33:11") to total seconds
+ * @param {string} get
+ * @returns {number}
+ */
+
 export function secondsFromGet(get) {
 	const [hh, mm, ss] = get.split(':').map(Number);
 	return hh * 3600 + mm * 60 + ss;
 }
 
+/**
+ * Comparator for sorting GET strings chronologically
+ * @param {string} a
+ * @param {string} b
+ * @returns {number}
+ */
 export function compareGET(a, b) {
 	return secondsFromGet(a) - secondsFromGet(b);
 }

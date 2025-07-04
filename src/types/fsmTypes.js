df2ad641c75a76beae950cccb17634cccadcc5ad
yenmangu <robert.shelford@googlemail.com>
2025-07-04 15:50:44 +0100

    Update fsm types

--- src/types/fsmTypes.js
+++ src/types/fsmTypes.js
@@ -1,10 +1,19 @@
 export {};
 
 /**
- * @typedef {import("../states/phases/basePhase.js").BasePhase} BasePhase
  * @typedef {import("./timelineTypes.js").PhaseId} PhaseId
- */
+ * @typedef {import ("../states/phases/basePhase.js").BasePhase} BasePhase
+ * @typedef {import("./runtimeTypes.js").RuntimePhase} RuntimePhase
+ * @typedef {import("../states/simulationState.js").SimulationState} SimulationState
+
+
 
 /**
- * @typedef {Object.<PhaseId, new (...args: any[]) => BasePhase>} PhaseRegistry
+ * @typedef {Record<
+ * 		PhaseId,
+ * 		new (
+ * 			simulationState: SimulationState,
+ * 			phaseMeta: RuntimePhase
+ * 		) => BasePhase
+ * >} PhaseRegistry
  */

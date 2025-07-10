35d1d4e703c5ebb83ef8fae9f7e2353bbe9e6980
yenmangu <robert.shelford@googlemail.com>
2025-07-10 13:05:47 +0100

    Update GET utility

--- src/util/GET.js
+++ src/util/GET.js
@@ -21,3 +21,10 @@ export function secondsFromGet(get) {
 export function compareGET(a, b) {
 	return secondsFromGet(a) - secondsFromGet(b);
 }
+
+export function formatSecondsToHHMMSS(seconds) {
+	const hh = String(Math.floor(seconds / 3600)).padStart(2, '0');
+	const mm = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
+	const ss = String(seconds % 60).padStart(2, '0');
+	return `${hh}:${mm}:${ss}`;
+}

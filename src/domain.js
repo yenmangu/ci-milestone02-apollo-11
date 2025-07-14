a135a8065deb38c6524b711b6aef6700b4cad72d
yenmangu <robert.shelford@googlemail.com>
2025-07-14 21:30:19 +0100

    Update domain logging

--- src/domain.js
+++ src/domain.js
@@ -1,13 +1,11 @@
 export default (function () {
-	console.log('domain.js');
-
 	const pathname = window.location.pathname;
 	const path = pathname.slice(pathname.lastIndexOf('/'));
 	const pageLinks = document.querySelectorAll('a.nav-link');
 	pageLinks.forEach(a => {
 		const anchor = /** @type {HTMLAnchorElement} */ (a);
 		const anchorPath = anchor.href.slice(anchor.href.lastIndexOf('/'));
-		console.log('path in domain: ', path);
+
 		if (anchorPath === path) {
 			anchor.classList.add('active');
 		} else {

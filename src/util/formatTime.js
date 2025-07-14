b0067ed81463be94f8331db45fb4760c530133ff
yenmangu <robert.shelford@googlemail.com>
2025-07-14 15:04:59 +0100

    Update utils

--- src/util/formatTime.js
+++
@@ -1,6 +0,0 @@
-export function formatSecondsToHHMMSS(seconds) {
-	const hh = String(Math.floor(seconds / 3600)).padStart(2, '0');
-	const mm = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
-	const ss = String(seconds % 60).padStart(2, '0');
-	return `${hh}${mm}${ss}`;
-}

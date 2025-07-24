bd7d7e82c8f4483d285cad7196a20207b3ee2d94
yenmangu <robert.shelford@googlemail.com>
2025-07-24 14:32:02 +0100

    Unsubscribe return updated

--- src/util/watchUntilComplete.js
+++ src/util/watchUntilComplete.js
@@ -1,9 +1,15 @@
 /**
  * @typedef {import('../types/runtimeTypes.js').RuntimeCue} RuntimeCue
  * @typedef {import('../types/runtimeTypes.js').NonTimeAction} RuntimeAction
+ * @typedef {import('../types/clockTypes.js').TickPayload} TickPayload
  */
 
-import { actionEmitter, agcEmitter, phaseEmitter } from '../event/eventBus.js';
+import {
+	actionEmitter,
+	agcEmitter,
+	phaseEmitter,
+	tickEmitter
+} from '../event/eventBus.js';
 
 /**
  * Subscribes to all 'action' and 'cue' events untill 'actionsComplete' is received.
@@ -13,11 +19,13 @@ import { actionEmitter, agcEmitter, phaseEmitter } from '../event/eventBus.js';
  * @param {(event: { [key: string]: RuntimeAction}) => void} onAction
  * @param {(event: { [key: string]: RuntimeCue}) => void} onCue
  * @param {(event: string) => void} onComplete
+ * @param {(tick:TickPayload) => void} onTick
  * @returns {{unsubscribe: () => void}}
  */
-export function watchUntilComplete(onAction, onCue, onComplete) {
+export function watchUntilComplete(onAction, onCue, onComplete, onTick) {
 	const actionSub = actionEmitter.on('action', onAction);
 	const cueSub = actionEmitter.on('cue', onCue);
+	const tickSub = tickEmitter.on('tick', onTick);
 	const completeSub = actionEmitter.on(
 		'actionsComplete',
 		(/** @type {string} */ event) => {
@@ -30,6 +38,7 @@ export function watchUntilComplete(onAction, onCue, onComplete) {
 		unsubscribe: () => {
 			actionSub.unsubscribe();
 			cueSub.unsubscribe();
+			tickSub.unsubscribe();
 			completeSub.unsubscribe();
 		}
 	};
